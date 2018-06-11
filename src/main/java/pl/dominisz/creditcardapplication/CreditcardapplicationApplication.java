b705fb81ef46d67efff4893824f89b41064c343b
dominisz <dominisz@gmail.com>
2018-06-11 20:45:01 +0200

    Initial commit

---
+++ src/main/java/pl/dominisz/creditcardapplication/CreditcardapplicationApplication.java
@@ -0,0 +1,12 @@
+package pl.dominisz.creditcardapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditcardapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditcardapplicationApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/dominisz/creditcardapplication/CreditcardapplicationApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.dominisz.creditcardapplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CreditcardapplicationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
