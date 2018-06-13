012675eb660aea661168f4147dad9f313822428d
dominisz <dominisz@gmail.com>
2018-06-13 20:56:52 +0200

    Show all credit cards

--- src/main/java/pl/dominisz/creditcardapplication/mvccontroller/MvcCreditCardEntityController.java
+++ src/main/java/pl/dominisz/creditcardapplication/mvccontroller/MvcCreditCardEntityController.java
@@ -1,7 +1,10 @@
 package pl.dominisz.creditcardapplication.mvccontroller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.dominisz.creditcardapplication.service.CreditCardEntityService;
 
 /**
  * http://dominisz.pl
@@ -11,5 +14,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/mvc/creditCards")
 public class MvcCreditCardEntityController {
 
+    private final CreditCardEntityService creditCardEntityService;
+
+    public MvcCreditCardEntityController(CreditCardEntityService creditCardEntityService) {
+        this.creditCardEntityService = creditCardEntityService;
+    }
+
+    @GetMapping
+    public String findAll(Model model) {
+        model.addAttribute("creditCards", creditCardEntityService.findAll());
+        return "creditCardList";
+    }
 
 }
