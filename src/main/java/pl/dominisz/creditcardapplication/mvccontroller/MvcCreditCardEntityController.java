003718b7e1d8e4c17799d012474cd01efdeedbaf
dominisz <dominisz@gmail.com>
2018-06-13 20:43:37 +0200

    Add MVC controller for credit cards

---
+++ src/main/java/pl/dominisz/creditcardapplication/mvccontroller/MvcCreditCardEntityController.java
@@ -0,0 +1,15 @@
+package pl.dominisz.creditcardapplication.mvccontroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * http://dominisz.pl
+ * 13.06.2018
+ */
+@Controller
+@RequestMapping("/mvc/creditCards")
+public class MvcCreditCardEntityController {
+
+
+}
