6bb8f1c35db481942cf2d1775f6b4c2f4bae8d29
dominisz <dominisz@gmail.com>
2018-06-14 18:30:48 +0200

    Show one card

--- src/main/java/pl/dominisz/creditcardapplication/mvccontroller/MvcCreditCardEntityController.java
+++ src/main/java/pl/dominisz/creditcardapplication/mvccontroller/MvcCreditCardEntityController.java
@@ -3,7 +3,9 @@ package pl.dominisz.creditcardapplication.mvccontroller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.dominisz.creditcardapplication.model.CreditCardEntity;
 import pl.dominisz.creditcardapplication.service.CreditCardEntityService;
 
 /**
@@ -24,6 +26,12 @@ public class MvcCreditCardEntityController {
     public String findAll(Model model) {
         model.addAttribute("creditCards", creditCardEntityService.findAll());
         return "creditCardList";
+    }
+
+    @GetMapping(path = "/{number}")
+    public String findCardByNumber(@PathVariable String number, Model model) {
+        model.addAttribute("creditCard", creditCardEntityService.findByNumber(number));
+        return "creditCard";
     }
 
 }
